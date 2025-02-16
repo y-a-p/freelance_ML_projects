1bd4bea2de78011603ab887f2dd1cd7e0a01c440
Yuri Polatovsky <yuri.polatovsky@yandex.ru>
2025-02-16 16:49:46 +0400

    Update app.py

--- 04_task_classification/app.py
+++ 04_task_classification/app.py
@@ -138,11 +138,11 @@ if st.button("Предсказать"):
 
         # Получаем предсказание для уровня 0
         level0_pred = final_model0.predict(X)
-        st.success(f"level0_pred {level0_pred}")
+        st.success(f"level0_pred {level0_pred[0]}")
         # Используем маршрутизатор для получения подтемы (уровень 1)
         level1_pred = predict_router_final(final_model0, final_model_math, final_model_phys,
                                            X, math_val, phys_val, math_le_final, phys_le_final)
-        st.success(f"level1_pred {level1_pred}")
+        st.success(f"level1_pred {level1_pred[0]}")
         # Декодируем числовые идентификаторы в наименования тем, используя таблицу topic_df.
         # Предполагается, что в таблице topic_df есть столбцы 'id' и 'name' для уровня 0 и уровня 1.
         # Если используется одна таблица для обоих уровней, то может потребоваться различать их по диапазону id
