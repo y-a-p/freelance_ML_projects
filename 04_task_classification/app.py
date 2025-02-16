58bdd177435cef649e18310755e9c1259c240c61
Yuri Polatovsky <yuri.polatovsky@yandex.ru>
2025-02-16 18:00:16 +0400

    Update app.py

--- 04_task_classification/app.py
+++ 04_task_classification/app.py
@@ -170,7 +170,7 @@ if st.button("Предсказать"):
         
         # Декодируем результаты
         st.success(f'dftype:{type(topic_df['id'][0])}')
-        st.success(f'encoder: {type(le_full.inverse_transform([level0_pred[0]])[0])}')
+        st.success(f'encoder1: {le_full.inverse_transform([level0_pred[0]])[0]}')
         topic_level0_name = decode_topic(int(le_full.inverse_transform([level0_pred[0]])[0]))
         st.success(f"topic_level0_name {topic_level0_name}")
         topic_level1_name = decode_topic(int(le_full.inverse_transform([level1_pred[0]])[0]))
